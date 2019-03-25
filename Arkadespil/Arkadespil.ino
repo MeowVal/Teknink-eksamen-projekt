a815ed4e1ce0a4f66d2173a6e6abc5c589a95153
Marc Hansen <marc@sabater.dk>
2019-03-25 08:54:03 +0100

    somthing?

--- Arkadespil/Arkadespil.ino
+++ Arkadespil/Arkadespil.ino
@@ -1,5 +1,5 @@
 
-const int dly = 10;
+const int dly = 100;
 const int dl = 1; 
 const int inputXPin = 10;
 const int inputYPin = 9;
@@ -35,13 +35,32 @@ void setup() {
   pinMode(inputYPin, INPUT);
   pinMode(inputKnapPin, INPUT);
   Serial.begin (9600);
+  red1 = B11111110;
+  green1 = B11111110;
+  blue4 = B11111110;
+  row1 = B00000001;
 }
 
 void loop() {
 
+red1 = red1<<1;
+red2 = red2<<1;
+red3 = red3<<1;
+red4 = red4<<1;
+green1 = green1<<1;
+green2 = green2<<1;
+green3 = green3<<1;
+green4 = green4<<1;
+blue1 = blue4<<1;
+blue2 = blue4<<2;
+blue3 = blue4<<3;
+blue4 = blue4<<4;
+row1 = row1<<1;
+row2 = row2<<2;
+row3 = row3<<3;
+row4 = row4<<4;
 
 
-  
     digitalWrite(latchPin,LOW);
     delayMicroseconds(dly);
     shiftOut(dataPin,clockPin,MSBFIRST,red1);
@@ -62,4 +81,6 @@ void loop() {
     shiftOut(dataPin,clockPin,MSBFIRST,row4);
     delayMicroseconds(dly);
     digitalWrite(latchPin,HIGH);
+    Serial.print("red1: ");
+    Serial.println(red1);
 }
